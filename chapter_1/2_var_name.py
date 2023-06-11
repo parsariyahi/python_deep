7d7e76b2ef3c53ae59eb4929ef470709fd1e747c
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-11 17:52:58 +0330

    ``chapter 1`` variable names and conventions for naming

---
+++ chapter_1/2_var_name.py
@@ -0,0 +1,13 @@
+# a var that starts with a "_" is
+# a intternal used var or private var.
+_var = "some value"
+
+
+# Used to mangle class attributes,
+# useful in inheritence chains
+__var = "some value"
+
+
+# used for system-defined names,
+# that have special meaning to interpreter.
+__var__ = "value"

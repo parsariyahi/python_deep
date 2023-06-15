8426eba7e500fa349ba1e255f5ba6f6f5b57fb5e
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-15 15:03:34 +0330

    ``chapter 1`` setter and getter for property in classes

--- chapter_1/8_classes.py
+++ chapter_1/8_classes.py
@@ -88,3 +88,42 @@ class Rectangle:
 
     def __repr__(self):
         return f"Rectange({self._width}, {self._height})"
+
+# Pythonic way to write getter and setter
+
+class Rectangle:
+
+    # Each time we run self.width or self.height,
+    # We run the getter and setter method.
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width <= 0:
+            raise ValueError("Width must be positive")
+        else:
+            self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        if height <= 0:
+            raise ValueError("Height must be positive")
+        else:
+            self._height = height
+
+    def __str__(self):
+        return f"Rectangle width={self._width}, height={self._height}"
+
+    def __repr__(self):
+        return f"Rectange({self._width}, {self._height})"

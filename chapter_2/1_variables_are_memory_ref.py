f6408c35f411dd5f212c3a9cfa753f26368a5634
parsariyahi <pany.parsariyahi@gmail.com>
2023-06-26 12:01:00 +0330

    ``chapter 2`` variables are memory refrences

---
+++ chapter_2/1_variables_are_memory_ref.py
@@ -0,0 +1,9 @@
+some_var = 10
+
+print(id(some_var)) #The memory address
+print(hex(id(some_var))) #The memory address HEX version
+
+some_str = "hello"
+
+print(id(some_str)) #The memory address
+print(hex(id(some_str))) #The memory address HEX version

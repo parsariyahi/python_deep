facb73530a95561282d39216e1556cb96f41d99a
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-31 16:26:09 +0330

    ``chapter 2`` variable equality

---
+++ chapter_2/9_variable_equality.py
@@ -0,0 +1,41 @@
+a = 19
+b = 19
+c = b
+
+print(a is b)
+print(a is c)
+print(b is c)
+
+
+a = 400
+b = 400
+print(a is b)
+print(a == b)
+
+
+a = "hello"
+b = "hello"
+print(a is b)
+print(a == b)
+
+a = "some longer string to check"
+b = "some longer string to check"
+print(a is b)
+print(a == b)
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a is b)
+print(a == b)
+
+
+print(id(None))
+
+
+a = None
+b = None
+c = None
+
+print(a is b)
+print(a is c)
+print(c is None)
